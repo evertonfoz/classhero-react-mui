b145538389e0df8970dc53c5a7244afd3369cb06
Everton Coimbra de Araújo <evertoncoimbra@gmail.com>
2025-06-07 23:55:39 -0300

    backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ClassesModule } from './classes/classes.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { DisciplinesModule } from './disciplines/disciplines.module';
+
 
 @Module({
-  imports: [ClassesModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+    AuthModule,
+    UsersModule,
+    CoursesModule,
+    DisciplinesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
